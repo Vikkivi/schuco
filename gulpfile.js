7481ea5d7b757eda0915d238f09d82f44ec4bb57
Vika <vikkivi147@gmail.com>
2018-03-10 20:40:30 +0300

    Шаблон сборки

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+
+gulp.task("style", function() {
+  gulp.src("sass/main.scss")
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("css"))
+    .pipe(server.stream());
+});
+
+gulp.task("serve", ["style"], function() {
+  server.init({
+    server: ".",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("*.html").on("change", server.reload);
+});
+
+gulp.task('default', ['serve']);
