4cbec17e8283188bd591ab427ab091326c2a8e9b
Vika <vikkivi147@gmail.com>
2018-03-12 20:18:01 +0300

    В gulp файл добавлена обработка js

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,31 @@ var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+
+gulp.task('common-js', function() {
+	return gulp.src([
+		'js/common.js',
+		])
+	.pipe(concat('common.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('js'));
+});
+
+gulp.task('js', ['common-js'], function() {
+	return gulp.src([
+		'libs/jquery/dist/jquery.min.js',
+		'js/common.min.js', // Всегда в конце
+		])
+	.pipe(concat('scripts.min.js'))
+	// .pipe(uglify()) // Минимизировать весь js (на выбор)
+	.pipe(gulp.dest('js'))
+	.pipe(server.reload({stream: true}));
+});
+
+
 
 gulp.task("style", function() {
   gulp.src("sass/main.scss")
@@ -26,6 +51,7 @@ gulp.task("serve", ["style"], function() {
   });
 
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch(['libs/**/*.js', 'js/common.js'], ['js']);
   gulp.watch("*.html").on("change", server.reload);
 });
 
--- js/common.js
+++ js/common.js
@@ -1,5 +1,16 @@
 $(function() {
+    
 
-	// Custom JS
+
+	 $(document).ready(function(){
+    $(".menu__nav, .header__content").on("click","a", function (event) {
+        console.log("hi");
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+    
 
 });
